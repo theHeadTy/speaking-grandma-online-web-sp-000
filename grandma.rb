f397fee246489dd4030cdc884c1a822f7682a269
generic-puppet-7755 <tasmith1990@gmail.com>
2019-11-01 21:25:00 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -1,13 +1,16 @@
-# Write a speak_to_grandma method.
+def speak_to_grandma(string)
+  case string
+  when 'I LOVE YOU GRANDMA!'
+    'I LOVE YOU TOO PUMPKIN!'
+  when 'Hi Nana, how are you?'
+    'HUH?! SPEAK UP, SONNY!'
+  when 'Hi!'
+    'HUH?! SPEAK UP, SONNY!'
+  when 'WHAT DID YOU EAT TODAY?'
+    'NO, NOT SINCE 1938!'
+  when 'WHAT?'
+    'NO, NOT SINCE 1938!'
+  end
+end
 
-# Whatever you say to grandma, she should respond with
-# HUH?! SPEAK UP, SONNY!
-# unless you shout it (type in all capitals).
-
-# If you shout, she can hear you (or at least she thinks so) 
-# and yells back
-
-# NO, NOT SINCE 1938!
-
-# However if you say 'I LOVE YOU GRANDMA!', she should respond with
-# 'I LOVE YOU TOO PUMPKIN!'
+speak_to_grandma('I LOVE YOU GRANDMA!')
